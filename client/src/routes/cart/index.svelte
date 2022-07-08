0beda8aae47bf6a1e29098bca741b855131362c6
Kwan Lee <kwan@saksoff5th.com>
2022-07-08 17:58:57 -0400

    Bag page prototyped

--- client/src/routes/cart/index.svelte
+++ client/src/routes/cart/index.svelte
@@ -1,1 +1,21 @@
+<script>
+	import Bag from '$lib/shopping/Bag.svelte';
+	import Wishlist from '$lib/shopping/Wishlist.svelte';
+</script>
+
 <div class="cart">This is cart page</div>
+
+<!-- ordered items -->
+<Bag />
+
+<!-- wishlist items, total items -->
+<Wishlist />
+
+<!-- total price -->
+<style>
+	.cart {
+		font-size: 1.2em;
+		font-style: italic;
+		margin-bottom: 10px;
+	}
+</style>
--- client/src/routes/checkout/index.svelte
+++ client/src/routes/checkout/index.svelte
@@ -1,1 +1,13 @@
 <div class="checkout">This is checkout page</div>
+
+<!-- order list -->
+
+<!-- shipping address -->
+
+<!-- payment choice -->
+
+<!-- promos -->
+
+<!-- loyalty ad -->
+
+<!-- other optional checkouts like Shoprunner -->
